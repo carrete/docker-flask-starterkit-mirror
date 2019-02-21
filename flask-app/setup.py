a576b19dac9a934437d643c8762f36afcf6455ee
Tom Vaughan <thomas.david.vaughan@gmail.com>
2019-02-21 06:46:13 +0000

    Update dependencies.

--- flask-app/setup.py
+++ flask-app/setup.py
@@ -13,16 +13,16 @@ meta = {
     "install_requires": [
         # Flask Starterkit
         "blinker==1.4",
-        "flask-admin==1.5.3",
+        "flask-admin==1.5.4",
         "flask-babel==0.12.2",
-        "flask-migrate==2.3.1",
+        "flask-migrate==2.5.2",
         "flask-script==2.0.6",
-        "flask-sqlalchemy==2.3.2",
-        "flask==1.0.2",
-        "psycopg2-binary==2.7.6.1",
-        "pyyaml==3.13",
+        "flask-sqlalchemy==2.4.1",
+        "flask==1.1.1",
+        "psycopg2-binary==2.8.4",
+        "pyyaml==5.2",
         "raven[flask]==6.10.0",
-        "sqlalchemy-utils==0.33.10",
+        "sqlalchemy-utils==0.36.0",
     ],
 }
 
